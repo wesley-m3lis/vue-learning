58175340dd3cce8e10f49d5f62dcbc31fe2116d8
wesley-m3lis <wesley.m3lis@gmail.com>
2022-07-09 00:34:40 -0300

    methods introduction

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+// Simply writes date and time and displays an About me link.
+
 const app = Vue.createApp({
     data(){
         return{
-            courseGoal: 'Learn some Vue and JS'
+            courseGoal: 'Learn some Vue and JS',
+            aboutMe: 'https://br.pinterest.com/wesleyameixoeira/'
         };
+    },
+
+    methods: {
+        date(){
+            return Date()
+        }
     }
 });
 
