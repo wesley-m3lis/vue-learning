7b5ba79ee72a7beb13a15917f519c69d0b0703a0
wesley-m3lis <wesley-m3lis@gmail.com>
2022-07-11 21:26:32 -0300

    myName attribute value

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const app = Vue.createApp({
     data(){
         return{
             image: 'https://i.pinimg.com/originals/5c/26/52/5c265259f626f75da99e9fb79c345ab8.png',
-            value: 'Type here',
             myName: "Wesley",
             myAge: 36
         }
@@ -14,8 +13,7 @@ const app = Vue.createApp({
         },
 
         randomNumber(){
-            const randomNumber = Math.random().toString().slice(0.0 , 1.0)
-            return randomNumber
+            return Math.random();
         }
     }
 })
