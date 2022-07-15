75eaa78f220003e7e87ef684ec651b2acf119d18
wesley-m3lis <wesley.m3lis@gmail.com>
2022-07-15 19:25:46 -0300

    type event added

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0
+  data(){
+    return{
+      counter: 0,
+      name: ''
+    }
+  },
+
+  methods : {
+    increments(num){
+      this.counter = this.counter + num
+    },
+
+    decrements(num){
+      this.counter = this.counter - num
+    },
+
+    setName(event){
+      this.name = event.target.value
     }
   }
-});
+})
 
 app.mount('#events');
